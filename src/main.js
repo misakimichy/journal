f342d7373c7eb5600836ff166e2c3c80c15de68c
Student <example@example.com>
2019-09-03 10:53:42 -0700

    Install jquery and bootstrap.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { journal } from './journal.js';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
