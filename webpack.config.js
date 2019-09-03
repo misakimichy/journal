d196c28b806189c53a9b4c0947f6b0da09289b94
Student <example@example.com>
2019-09-03 10:08:27 -0700

    Bundle css.

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,17 @@ module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__diename, 'dist')
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   }
 };
