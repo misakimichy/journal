00d5a0869f67c3e42051eb63fdb04ed95f3e7148
Student <example@example.com>
2019-09-03 10:19:14 -0700

    Add HtmlWebpackPlugin and CleanWebpackPlugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
@@ -6,6 +8,14 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Journal',
+      template:'./src/index.html',
+      inject: 'body'
+    })
+  ],
   module: {
     rules: [
       {
